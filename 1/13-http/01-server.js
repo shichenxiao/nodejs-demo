def801e17937ea6e7f3aceebf005248698b624cf
shichenxiao <1739118128@qq.com>
2017-10-19 19:30:45 +0800

    添加http文件

---
+++ 1/13-http/01-server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+var server = require('http').createServer();
+
+server.on('request',function(req,res){
+  console.log("REQUEST:"+JSON.stringify(req.headers));
+  req.pipe(process.stdout);
+  res.end("hello world");
+});
+
+server.listen(8080,function(){
+  console.log("listing on %d",this.address().port);
+});
